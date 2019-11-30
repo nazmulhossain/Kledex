058d6f25ddfa475eefe6de36ea6f3efe5dea23b2
Luca Briguglia <lubrigug@microsoft.com>
2019-11-30 21:48:23 +0000

    Update service bus message factory

--- src/Kledex.Bus.ServiceBus/Factories/MessageFactory.cs
+++ src/Kledex.Bus.ServiceBus/Factories/MessageFactory.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 using Microsoft.Azure.ServiceBus;
-using Newtonsoft.Json;
 
 namespace Kledex.Bus.ServiceBus.Factories
 {
     public class MessageFactory : IMessageFactory
     {
         public static readonly string AssemblyQualifiedNamePropertyName = "AssemblyQualifiedName";
+
         /// <inheritdoc />
         public Message CreateMessage<TMessage>(TMessage message) where TMessage : IBusMessage
         {
-            var json = JsonConvert.SerializeObject(message);
+            var json = JsonSerializer.Serialize(message);
             var serviceBusMessage = new Message(Encoding.UTF8.GetBytes(json))
             {
                 ContentType = "application/json"
@@ -21,26 +23,32 @@ namespace Kledex.Bus.ServiceBus.Factories
             {
                 foreach (var prop in message.Properties)
                 {
+                    var mProp = serviceBusMessage.GetType().GetProperty(prop.Key);
+                    if (mProp != null)
+                    {
+                        //Type mPropoType = mProp.GetType();
+                        mProp.SetValue(mProp, message.Properties[prop.Key]);
+                    }
+                    else
+                    {
+                        serviceBusMessage.UserProperties.Add(prop.Key, prop.Value);
+                    }
+
+
                     // We could use reflexion here, but i believe we should bet on performace and simplicity.
                     // If not, then we can consider adding more of this properties
-                    if (prop.Key == nameof(serviceBusMessage.Label))
-                        serviceBusMessage.Label = message.Properties[prop.Key] as string;
-                    else if (prop.Key == nameof(serviceBusMessage.SessionId))
-                        serviceBusMessage.SessionId = message.Properties[prop.Key] as string;
-                    else if (prop.Key == nameof(serviceBusMessage.CorrelationId))
-                        serviceBusMessage.CorrelationId = message.Properties[prop.Key] as string;
-                    else if (prop.Key == nameof(serviceBusMessage.ScheduledEnqueueTimeUtc) && message.Properties[prop.Key] is System.DateTime ScheduledEnqueueTimeUtc)
-                        serviceBusMessage.ScheduledEnqueueTimeUtc = ScheduledEnqueueTimeUtc;
-                    else
-                        serviceBusMessage.UserProperties.Add(prop.Key, prop.Value);
+                    //if (prop.Key == nameof(serviceBusMessage.Label))
+                    //    serviceBusMessage.Label = message.Properties[prop.Key] as string;
+                    //else if (prop.Key == nameof(serviceBusMessage.SessionId))
+                    //    serviceBusMessage.SessionId = message.Properties[prop.Key] as string;
+                    //else if (prop.Key == nameof(serviceBusMessage.CorrelationId))
+                    //    serviceBusMessage.CorrelationId = message.Properties[prop.Key] as string;
+                    //else if (prop.Key == nameof(serviceBusMessage.ScheduledEnqueueTimeUtc) && message.Properties[prop.Key] is System.DateTime ScheduledEnqueueTimeUtc)
+                    //    serviceBusMessage.ScheduledEnqueueTimeUtc = ScheduledEnqueueTimeUtc;
+                    //else
+                    //    serviceBusMessage.UserProperties.Add(prop.Key, prop.Value);
                 }
             }
-
-            //NOTE: we keep prop mapping this for legacy users, it's been marked as obsolete and should be removed in a future release
-            if (message.ScheduledEnqueueTimeUtc.HasValue
-                && message.Properties != null
-                && !message.Properties.ContainsKey(nameof(message.ScheduledEnqueueTimeUtc)) )
-                serviceBusMessage.ScheduledEnqueueTimeUtc = message.ScheduledEnqueueTimeUtc.Value;
 
             serviceBusMessage.UserProperties.Add(new KeyValuePair<string, object>(AssemblyQualifiedNamePropertyName, message.GetType().AssemblyQualifiedName));
 
